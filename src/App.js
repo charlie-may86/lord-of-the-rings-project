e52b4fe7642638157a899012f08a858496305b6c
Charles May <charlie@Charless-MacBook-Pro.local>
2021-06-15 07:53:18 -0500

    axios request working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "./styles.scss";
 
 //Components
 import Header from "./components/Header";
 import MovieCard from "./components/MovieCard";
-import MovieSelector from './components/MovieSelector'
+import MovieSelector from "./components/MovieSelector";
+
+const token = "eSN8I7cd6_NAGmdIXKEK";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+
+  useEffect(() => {
+    console.log("this is the useEffect");
+    axios
+      .get("https://the-one-api.dev/v2/movie", config)
+      .then((res) => {
+        console.log(res.data.docs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div>
       <Header />
